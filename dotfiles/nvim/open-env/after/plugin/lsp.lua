66fbea0e756c6d3f62ca0fc9d16e19d146f690e8
Daniel R. Koris <korisd@gmail.com>
2023-11-25 11:49:26 -0700

    changed bindings for completions
    This just feel a little more natural to me.

--- dotfiles/nvim/open-env/after/plugin/lsp.lua
+++ dotfiles/nvim/open-env/after/plugin/lsp.lua
@@ -87,9 +87,9 @@ cmp.setup({
     completeopt = 'menu,menuone,noinsert'
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-k>'] = cmp.mapping.select_next_item(),
+    ['<C-i>'] = cmp.mapping.select_prev_item(),
+--    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
