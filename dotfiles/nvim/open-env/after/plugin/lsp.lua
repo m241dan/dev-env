3ebcb43014a8c30152fc494753a03721c9e404d4
Daniel R. Koris <korisd@gmail.com>
2023-12-24 11:29:06 -0700

    added some missing things to my lsp config file

--- dotfiles/nvim/open-env/after/plugin/lsp.lua
+++ dotfiles/nvim/open-env/after/plugin/lsp.lua
@@ -60,6 +60,8 @@ lspconfig.lua_ls.setup({
 
 -- Setup Clangd
 lspconfig.clangd.setup({
+    -- Neovim specific Params
+    on_attach = on_attach,
     cmd = {
         "clangd",
         "--query-driver='/usr/bin/g++'",
@@ -93,8 +95,8 @@ cmp.setup({
     completeopt = 'menu,menuone,noinsert'
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-k>'] = cmp.mapping.select_next_item(),
-    ['<C-i>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
 --    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
