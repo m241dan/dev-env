73907b8fdc55e2147bb571e607264928e253063c
Daniel R. Koris <korisd@gmail.com>
2023-12-27 09:56:13 -0700

    changed my window handling remaps
    These were missed when I changed remaps from switching back to
    h,j,k,l.

--- dotfiles/nvim/open-env/lua/mappings.lua
+++ dotfiles/nvim/open-env/lua/mappings.lua
@@ -12,14 +12,14 @@ vim.keymap.set('n', '<leader>di', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>di', "<cmd>lua vim.diagnostic.goto_prev()<CR><Insert>")
 vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>dK', "<cmd>lua vim.diagnostic.goto_next()<CR><Insert>")
-vim.keymap.set('n', '<C-i>', "<cmd>res +1<CR>") -- Grow current width height wise
-vim.keymap.set('n', '<C-k>', "<cmd>res -1<CR>") -- Shrink current width height wise
+vim.keymap.set('n', '<C-k>', "<cmd>res +1<CR>") -- Grow current width height wise
+vim.keymap.set('n', '<C-j>', "<cmd>res -1<CR>") -- Shrink current width height wise
 vim.keymap.set('n', '<C-l>', "<cmd>vert res +1<CR>") -- Grow current window width wise
-vim.keymap.set('n', '<C-j>', "<cmd>vert res -1<CR>") -- Shrink current window width wise
-vim.keymap.set('n', '<leader>wi', "<C-W>k") -- Move to the window above of the current
-vim.keymap.set('n', '<leader>wk', "<C-W>j") -- Move to the window below of the current
+vim.keymap.set('n', '<C-h>', "<cmd>vert res -1<CR>") -- Shrink current window width wise
+vim.keymap.set('n', '<leader>wk', "<C-W>k") -- Move to the window above of the current
+vim.keymap.set('n', '<leader>wj', "<C-W>j") -- Move to the window below of the current
 vim.keymap.set('n', '<leader>wl', "<C-W>l") -- Move to the window right of the current
-vim.keymap.set('n', '<leader>wj', "<C-W>h") -- Move to the window left of the current
+vim.keymap.set('n', '<leader>wh', "<C-W>h") -- Move to the window left of the current
 
 -- Insert mode keymaps
 vim.keymap.set('i', '<C-D>', "<Esc>")
