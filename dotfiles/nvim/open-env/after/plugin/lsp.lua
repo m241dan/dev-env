34023e2d0f722d5a9c7773e3ec5d53f1064ddabc
Daniel R. Koris <korisd@gmail.com>
2024-01-06 12:22:27 -0700

    changed a lot of my lsp bindings to match the existing leader + l pattern

--- dotfiles/nvim/open-env/after/plugin/lsp.lua
+++ dotfiles/nvim/open-env/after/plugin/lsp.lua
@@ -15,24 +15,24 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>lrn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>lca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>lgd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+  nmap('<leader>lgr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('<leader>lgI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  nmap('<leader>lD', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+  nmap('<leader>lds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>lws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help H` for why this keymap
-  nmap('H', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-h>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  nmap('<leader>lh', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<leader>lH', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>lgD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>lwa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>lwr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
 --  nmap('<leader>wl', function()
 --    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 --  end, '[W]orkspace [L]ist Folders')
