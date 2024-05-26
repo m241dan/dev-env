28fda039f6879fe0f1b65eba23710cd7b28265fb
Daniel R Koris <korisd@gmail.com>
2024-05-26 11:57:20 -0600

    added a setup-env bash script
    I don't think I actually want to symlink the external code
    that I'm not really going to update, just the dotfiles. But
    saving this for now...

---
+++ setup-env.sh
@@ -0,0 +1,73 @@
+#/bin/bash
+
+set -e
+
+#
+# Helper functions
+#
+symlink_folder_contents() {
+    local target_dir="$1"
+    local output_dir="$2"
+    local type="$3"
+
+    for plugin in "$target_dir"/*/; do
+        # do some fancy bash bullshit to get the paths right
+        plugin=${plugin%*/} # e.g. turn "plugins/tmux/tpm2k/" to "plugins/tmux/tpm2k"
+        plugin=${plugin##*/} # e.g. turn "plugins/tmux/tpm2k" to "tpm2k"
+        echo "Symlinking ${type} ${plugin}"
+        ln -s $target_dir/$plugin $output_dir/$plugin
+    done
+}
+
+#
+# Real script actions starts here
+#
+PATH_TO_REPO="${PATH_TO_REPO:-$PWD}"
+OHMYZSHHOME="${OHMYZSHHOME:-$HOME/.oh-my-zsh}"
+TMUX_PLUGINS="${TMUX_PLUGINS:-$HOME/.tmux_plugins}"
+HOME_SHARE="${HOME_SHARE:-$HOME/.local/share}"
+HOME_CONFIG="${HOME_CONFIG:-$HOME/.config}"
+NVIM_CONFIG="${NVIM_CONFIG:-$HOME_CONFIG/nvim}"
+NVIM_SHARE="${NVIM_SHARE:-$HOME_SHARE/nvim}"
+NVIM_PLUGINS="${NVIM_PLUGINS:-$NVIM_SHARE/plugins}"
+CLANGD_CONFIG="${CLANGD_CONFIG:-$HOME_CONFIG/clangd}"
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.zsh_plugins}"
+ZSH_THEMES="${ZSH_THEMES:-$ZSH_CUSTOM/themes}"
+
+
+# Create folders for what we're symlinking if the base roots don't already exist
+echo "Creating base folders for things that will be symlinked from the dev-env repo"
+
+mkdir -p $NVIM_SHARE
+mkdir -p $NVIM_CONFIG
+mkdir -p $NVIM_PLUGINS
+mkdir -p $CLANGD_CONFIG 
+mkdir -p $TMUX_PLUGINS
+mkdir -p $ZSH_THEMES
+
+# Symlink all files that will be mounted to local locations
+
+echo "Symlinking files in the dev repository..."
+echo "This will allow for changes in local or in a container to be synced by to the repository easily"
+
+# Software
+ln -s $PATH_TO_REPO/software/ohmyzsh $OHMYZSHHOME
+
+# Plugins
+symlink_folder_contents "$PATH_TO_REPO/plugins/nvim" $NVIM_PLUGINS "plugin"
+symlink_folder_contents "$PATH_TO_REPO/plugins/tmux" $TMUX_PLUGINS "plugin"
+
+# Themes
+symlink_folder_contents "$PATH_TO_REPO/themes/zsh" $ZSH_THEMES "theme"
+symlink_folder_contents "$PATH_TO_REPO/themes/tmux" $TMUX_PLUGINS "theme"
+
+# Dotfiles
+ln -s $PATH_TO_REPO/dotfiles/nvim/open-env $NVIM_CONFIG
+ln -s $PATH_TO_REPO/dotfiles/zsh/zshrc $HOME/.zshrc
+ln -s $PATH_TO_REPO/dotfiles/zsh/p10k.zsh $HOME/.p10k.zsh
+ln -s $PATH_TO_REPO/dotfiles/zsh/aliases $HOME/.aliases
+ln -s $PATH_TO_REPO/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $PATH_TO_REPO/dotfiles/clangd/config.yaml $HOME/.config/clangd/config.yaml
+
+
+
