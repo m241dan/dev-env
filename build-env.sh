c4f9789eb4e5a5d0af3adf54a80de5708b066dfa
Daniel R. Koris <korisd@gmail.com>
2024-10-05 23:50:46 +0000

    changed build-env to use the system's USER if not specified

--- build-env.sh
+++ build-env.sh
@@ -1,2 +1,8 @@
+set -ex
+
 IMAGE="${IMAGE:-new-env:latest}"
-podman build . -t $IMAGE -f new-env.Dockerfile
+if [ -z "$IMG_USER" ]; then
+  IMG_USER="$USER"
+fi
+# podman build . -f new-env.Dockerfile --target build-env
+podman build --format=docker --build-arg USER=$IMG_USER . -t $IMAGE -f new-env.Dockerfile
